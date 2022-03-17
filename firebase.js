2a8ce8a56f3a543a4a07860a627823bdedcf2e76
Robin Ruf <robin.ruf@hotmail.com>
2022-03-18 00:38:59 +0100

    Initialize Clone Project

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAvKQ_Mf1gh-wrmv6Xtwxyjo1sDKb1JrGk",
+    authDomain: "docsclone-92966.firebaseapp.com",
+    projectId: "docsclone-92966",
+    storageBucket: "docsclone-92966.appspot.com",
+    messagingSenderId: "887364392063",
+    appId: "1:887364392063:web:e06ac07bdacd2044b65b4a"
+  };
+
+const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
+
+const db = app.firestore();
+
+export { db };
